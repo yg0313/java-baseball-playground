4f29590b47d7855915904cef21293fb2fc61cd31
Kim yonggwan <sssw0101@naver.com>
2022-03-31 01:21:41 +0900

    Update StringTest.java

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,13 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    public void subString() {
+        String input = "(1,2)";
+        String result = input.substring(1, input.length() - 1);
+        assertThat(5).isSameAs(input.length());
+        assertThat(result).isEqualTo("1,2");
+    }
+
 }
